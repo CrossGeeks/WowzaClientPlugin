7c294a3fb826b91367fa069194eb2d2fc61e6349
Luis Pujols <luispujols@hotmail.com>
2019-02-08 15:41:13 -0500

    Added the WowzaClientExceptions, and removed the project libraries for iOS and Android. to add through the new Binding Nugets

---
+++ WowzaClient/WowzaClient/WowzaClientException.shared.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.WowzaClient
+{
+    public class WowzaClientBaseException : Exception
+    {
+        public const string PlayerDefaultErrorMessage = "The Wowza Client player has not completed it's process correctly.";
+        public const string WowzaClientNotInitializedErrorMessage = "The Wowza Client is not initialized correctly.";
+        public const string WowzaClientNotSetupErrorMessage = "The Wowza Client is not setup.";
+        public const string WowzaClientPlayerViewNotAttachedErrorMessage = "The Wowza Client player view it not attached correctly.";
+        public const string WowzaClientLicensingKeyErrorMessage = "The Wowza Client key is not valid.";
+        public const string WowzaClientHslUrlParameterNullOrEmptyErrorMessage = "The HslUrl parameter is null or empty.";
+
+        public WowzaClientBaseException() : base(PlayerDefaultErrorMessage) { }
+        public WowzaClientBaseException(string message) : base(message) { }
+        public WowzaClientBaseException(string message, Exception inner) : base(PlayerDefaultErrorMessage, inner) { }
+    }
+
+    public class WowzaClientNotInitializedException : WowzaClientBaseException
+    {
+        public WowzaClientNotInitializedException() : base(WowzaClientNotInitializedErrorMessage) { }
+        public WowzaClientNotInitializedException(string message) : base(message) { }
+        public WowzaClientNotInitializedException(string message, Exception inner) : base(WowzaClientNotInitializedErrorMessage, inner) { }
+    }
+
+    public class WowzaClientNotSetupException : WowzaClientBaseException
+    {
+        public WowzaClientNotSetupException() : base(WowzaClientNotSetupErrorMessage) { }
+        public WowzaClientNotSetupException(string message) : base(message) { }
+        public WowzaClientNotSetupException(string message, Exception inner) : base(WowzaClientNotSetupErrorMessage, inner) { }
+    }
+
+    public class WowzaClientPlayerViewNotAttachedException : WowzaClientBaseException
+    {
+        public WowzaClientPlayerViewNotAttachedException() : base(WowzaClientPlayerViewNotAttachedErrorMessage) { }
+        public WowzaClientPlayerViewNotAttachedException(string message) : base(message) { }
+        public WowzaClientPlayerViewNotAttachedException(string message, Exception inner) : base(WowzaClientPlayerViewNotAttachedErrorMessage, inner) { }
+    }
+
+    public class WowzaClientLicensingKeyException : WowzaClientBaseException
+    {
+        public WowzaClientLicensingKeyException() : base(WowzaClientLicensingKeyErrorMessage) { }
+        public WowzaClientLicensingKeyException(string message) : base(message) { }
+        public WowzaClientLicensingKeyException(string message, Exception inner) : base(WowzaClientLicensingKeyErrorMessage, inner) { }
+    }
+
+    public class WowzaClientHslUrlParameterNullOrEmptyException : WowzaClientBaseException
+    {
+        public WowzaClientHslUrlParameterNullOrEmptyException() : base(WowzaClientHslUrlParameterNullOrEmptyErrorMessage) { }
+        public WowzaClientHslUrlParameterNullOrEmptyException(string message) : base(message) { }
+        public WowzaClientHslUrlParameterNullOrEmptyException(string message, Exception inner) : base(WowzaClientHslUrlParameterNullOrEmptyErrorMessage, inner) { }
+    }
+}
--- WowzaClient/WowzaClient/WowzaClientManager.apple.cs
+++ WowzaClient/WowzaClient/WowzaClientManager.apple.cs
@@ -1,36 +1,228 @@
-﻿using System;
+﻿using Foundation;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using UIKit;
+using Wowza;
 
 namespace Plugin.WowzaClient
 {
     /// <summary>
     /// Interface for WowzaClient
     /// </summary>
-    public class WowzaClientManager : IWowzaClientManager
+    public class WowzaClientManager : NSObject, IWowzaClientManager
     {
-        public float Volume { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        IWowzaConfig wowzaConfig;
+        public static WowzaConfig Config;
+        static WOWZPlayer Player;
+        WowzaClientStatusCallback wowzaClientStatusCallback = new WowzaClientStatusCallback();
 
-        public bool IsPlaying => throw new NotImplementedException();
+        public float Volume
+        {
+            get
+            {
+                return Player?.Volume ?? 0;
+            }
+            set
+            {
+                if (Player != null)
+                    Player.Volume = value;
+            }
+        }
 
-        public bool IsReadyToPlay => throw new NotImplementedException();
+        public bool IsPlaying
+        {
+            get
+            {
+                return Player?.Playing ?? false;
+            }
+        }
 
-        public event WOWZPlayerStatusChangedEventHandler OnPlayerStatusChanged;
-        public event WOWZErrorEventHandler OnError;
+        public bool IsReadyToPlay
+        {
+            get
+            {
+                return IsPlaying;
+            }
+        }
+
+        public WowzaClientManager()
+        {
+            if (Player == null)
+                throw new WowzaClientNotInitializedException();
+        }
+
+        public static void Initialize(string key)
+        {
+            var goCoderLicensingError = WowzaGoCoder.RegisterLicenseKey(key);
+            if (goCoderLicensingError != null)
+            {
+                // Log license key registration failure
+                Console.WriteLine(goCoderLicensingError.LocalizedDescription);
+                throw new WowzaClientLicensingKeyException(goCoderLicensingError.LocalizedDescription);
+            }
+
+            Player = new WOWZPlayer();
+        }
+
+
+        static WOWZPlayerStatusChangedEventHandler _OnPlayerStatusChanged;
+        public event WOWZPlayerStatusChangedEventHandler OnPlayerStatusChanged
+        {
+            add
+            {
+                _OnPlayerStatusChanged += value;
+            }
+            remove
+            {
+                _OnPlayerStatusChanged -= value;
+            }
+        }
+
+        static WOWZErrorEventHandler _OnError;
+        public event WOWZErrorEventHandler OnError
+        {
+            add
+            {
+                _OnError += value;
+            }
+            remove
+            {
+                _OnError -= value;
+            }
+        }
 
         public void Play()
         {
-            throw new NotImplementedException();
+            if (Config == null)
+                throw new WowzaClientNotSetupException();
+
+            if (Player.PlayerView == null)
+                throw new WowzaClientPlayerViewNotAttachedException();
+
+            if (!Player.Playing)
+            {
+                Player.Play(Config, wowzaClientStatusCallback);
+            }
+            else
+            {
+                Player.ResetPlaybackErrorCount();
+                Player.Stop();
+            }
         }
 
-        public void Setup(IWowzaConfig config)
+        public static void AttachPlayerView(UIView view)
         {
-            throw new NotImplementedException();
+            if (Config == null)
+                throw new WowzaClientNotSetupException();
+
+            Player.PlayerView = view;
+        }
+
+
+        public static void ResetPlaybackErrorCount()
+        {
+            Player.ResetPlaybackErrorCount();
+        }
+
+        public static void UpdateViewLayouts()
+        {
+            Player.UpdateViewLayouts();
+        }
+
+        public static void AllowHSLPlayback(bool allow, string hslUrl)
+        {
+            Config.AllowHLSPlayback = allow;
+
+            if (allow && string.IsNullOrEmpty(hslUrl))
+                throw new WowzaClientHslUrlParameterNullOrEmptyException();
+
+            Config.HlsURL = hslUrl;
         }
 
         public void Stop()
         {
-            throw new NotImplementedException();
+            if (Config == null)
+                throw new WowzaClientNotSetupException();
+
+            if (Player.PlayerView == null)
+                throw new WowzaClientPlayerViewNotAttachedException();
+
+            if (Player.Playing)
+                Player.Stop();
+
+        }
+
+        public void Setup(IWowzaConfig config)
+        {
+            if (config == null)
+                throw new ArgumentNullException();
+
+            Config = new WowzaConfig();
+            // Perform any additional setup after loading the view, typically from a nib.
+            Config.AudioEnabled = true;
+            //Config.VideoEnabled = true;
+
+            Config.HostAddress = config.HostAddress;
+            Config.PortNumber = (nuint)config.PortNumber;
+            Config.ApplicationName = config.ApplicationName;
+            Config.StreamName = config.StreamName;
+            Config.Username = config.Username;
+            Config.Password = config.Password;
+
+            WowzaGoCoder.SetLogLevel(WowzaGoCoderLogLevel.Verbose);
+
+            Console.WriteLine(string.Format("{0} {1} {2} {3}", WOWZVersionInfo.MajorVersion, WOWZVersionInfo.MinorVersion,
+                                            WOWZVersionInfo.Revision, WOWZVersionInfo.BuildNumber));
+        }
+
+        class WowzaClientStatusCallback : WOWZStatusCallback
+        {
+            public override void OnWOWZError(WOWZStatus status)
+            {
+                Player.Stop();
+                Player.ResetPlaybackErrorCount();
+                var errorArgs = new WOWZErrorEventHandlerEventArgs();
+                errorArgs.Message = status.Description;
+                _OnError?.Invoke(this, errorArgs);
+            }
+
+            public override void OnWOWZStatus(WOWZStatus status)
+            {
+                string statusMessage = ("Player status: ");
+                var statusArgs = new WOWZPlayerStatusChangedEventHandlerEventArgs();
+
+                switch (status.State)
+                {
+                    case WOWZState.Starting:
+                        statusArgs.State = WowzaClientStatus.Starting;
+                        statusMessage += ("Player initializing");
+                        break;
+
+                    case WOWZState.Ready:
+                        statusArgs.State = WowzaClientStatus.Ready;
+                        statusMessage += ("Ready to begin listening");
+                        break;
+
+                    case WOWZState.Running:
+                        statusArgs.State = WowzaClientStatus.Running;
+                        statusMessage += ("Player is active");
+                        break;
+
+                    case WOWZState.Stopping:
+                        statusArgs.State = WowzaClientStatus.Stopping;
+                        statusMessage += ("Player shutting down");
+                        break;
+
+                    case WOWZState.Idle:
+                        statusArgs.State = WowzaClientStatus.Idle;
+                        statusMessage += ("The Player is stopped");
+                        break;
+                }
+
+                statusArgs.Message = statusMessage;
+                _OnPlayerStatusChanged?.Invoke(this, statusArgs);
+            }
         }
     }
 }
